c4edfbd55fe221df918c23449975202c0d0c9d57
Mai <mk2e22@soton.ac.uk>
2024-06-28 11:37:57 +0900

    Modified some code

--- week4-Wikipedia/Wikipedia.java
+++ week4-Wikipedia/Wikipedia.java
@@ -11,6 +11,8 @@ public class Wikipedia {
     // A set of page links.
     private Map<Integer, List<Integer>> links = new HashMap<>();  // "12 342" <- 左のIDから飛ぶ先のID
 
+    public int mostPop;
+
 
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
@@ -140,7 +142,7 @@ public class Wikipedia {
             }
         }
         // Print titles in the path
-        // loop from the last index to skip the process of reversing order (which might be ignorable difference)
+        // Loop from the last index to skip the process of reversing order (which might be ignorable difference)
         for(int i = path.size()-1; i >= 0; i--){
             System.out.println(path.get(i));
             if(i != 0) System.out.println(", ");
@@ -164,20 +166,86 @@ public class Wikipedia {
 
     // Calculate the page ranks and print the most popular pages
     public void findMostPopularPages() {
-        int[] scores = new int[titles.size()];
+        int titlesListSize = titles.size();
+        float[] scores = new float[titlesListSize];
         Arrays.fill(scores, 1);  // Initialise scores list value to 1
 
+        while (true) {
+            float[] scoresToAdd = new float[titlesListSize];
+            for(int i = 0; i < titlesListSize; i++) {  // loop for each node
+                float score = scores[i];
+
+                List<Integer> neighbour = links.get(i);
+                if(neighbour != null) {
+                    // Distribute 85% of the score to its neighbour
+                    float addToNeighbour = (float) (score * 0.85 / neighbour.size());
+                    for (int index : neighbour) {
+                        scoresToAdd[index-1] += addToNeighbour;  //indexに0がなさそうなら+1をするべき
+                    }
+
+                    // Distribute 15% of its score to all
+                    float addToAll = (float) (score * 0.15 / (titlesListSize - 1));
+                    for(int j = 0; j < titlesListSize; j++){
+                        if(j != i) scoresToAdd[j] += addToAll;
+                    }
+                }else{
+                    // If the node does not have any neighbours, then Distribute its score to all
+                    float addToAll = (score / (titlesListSize - 1));
+                    for(int j = 0; j < titlesListSize; j++){
+                        if(j != i) scoresToAdd[j] += addToAll;
+                    }
+                }
+            }
+
+            float sum = 0;
+            // Update scores
+            for(int j = 0; j < titlesListSize; j++){
+                float old = scores[j];
+                scores[j] = scoresToAdd[j];
+
+                System.out.println("scores[] + scoresToAdd => " + old + " + " + scoresToAdd[j]);
+
+                sum += Math.abs(old - scores[j]);
+
+                System.out.println("old: " + old);
+                System.out.println("new: " + scores[j]);
+                System.out.println("sum: " + sum);
+            }
+
+            // Terminate loop when the sum of the changes in all scores became lower than 0.01
+            if (Math.sqrt(sum) < 0.01) break;
+            System.out.println(sum);
+
+            try {
+                Thread.sleep(10);  // 1秒（1000ミリ秒）間スリープする
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Find a node with the highest score
+        float max = Integer.MIN_VALUE;
+        mostPop = 0;
+        for (int i = 0; i < scores.length; i++) {
+            if (scores[i] > max) {
+                max = scores[i];
+                mostPop = i;
+            }
+        }
+
+        System.out.println("The most popular page is: " + titles.get(mostPop));
         // TODO:
         //  足されるvalueをまとめるための新リストを作成。scores[]*0.15/scores.lengthの値を格納する fif　を作成。
         //  各IDにおいて、linksを使用して"リンクされている数 n" と"リンク先ID"の割り出し、
         //  scores[]にあるvalue*0.85/n と　fif を　IDのvalueへ追加していく。
-        //  全リンク先IDをループできたらscoresに追加、収束済みか確認 -> false であれば再度ループ。do whileがいいな。
+        //  全リンク先IDをループできたらscoresに追加、収束済みか確認 -> false であれば再度ループ。do whileがいい。
         //  収束はscoresサイズのループにて　int syusoku += Math.sqrt(scores[] - newScores[]) が 0.01を下回ればtrueとする。
-
-        //これREADMEに写して
-
-
     }
+
+    public String getMostPopular(){
+        return titles.get(mostPop);
+    }
+
 
     // Do something more interesting!
     public void findSomethingMoreInteresting() {
--- week5-TSP/SalesMan.java
+++ week5-TSP/SalesMan.java
@@ -8,6 +8,8 @@ import java.util.Scanner;
 public class SalesMan {
 
     List<Point> pointList = new ArrayList<>();
+    List<Integer> points = new ArrayList<>();
+
 
     Node head;
 
@@ -19,8 +21,13 @@ public class SalesMan {
     public static void main(String[] args) {
         SalesMan salesMan = new SalesMan();
         salesMan.storeLocation();
+        float[][] distances = salesMan.storeDistance();
+        ArrayList<Integer> shortestPath = salesMan.findShortestPath(distances);
+        ArrayList<Integer> modifiedPath = salesMan.modifyPath(shortestPath, distances);
+
         List<Node> firstTSP = salesMan.greedyAlgoSearch();  //with some intersection
-        List<Node> fixedTSP = salesMan.removeIntersection(firstTSP);  //with any intersections removed
+        //List<Node> fixedTSP =
+        salesMan.removeIntersection(firstTSP);  //with any intersections removed
 
         // .....
     }
@@ -44,45 +51,147 @@ public class SalesMan {
         }
     }
 
-    public List<Node> removeIntersection(List<Node> nodes){
-        List<Node> nodeList = nodes;
-        Node current = head.nextNode;
-        Node next = current.nextNode;
+    public float[][] storeDistance(){
+        int l = pointList.size();
+        float[][] distances = new float[l][l];
 
-        // 交点が存在する限りループする。調査中の線分と他のすべての線分を比較、交点があれば繋ぎなおし。
-        // 繋ぎなおし方は寝る前の自分にまかせた。
-        // current と nextから[current.current & current.next]の座標を取得、別でnextも同様に。
-        // 次のノードへ移る際はnext = next.nextNodeで引き渡し。この時点では仮のwhile(true)の中に別ループがあるはず。
-        while(true){
-            Point p1 = pointList.get(current.current);
-            Point p2 = pointList.get(next.current);
-            while(!next.equals(current)) {
-                Point p3 = pointList.get(next.current);
-                Point p4 = pointList.get(next.nextNode.current);
+        for(int i = 0; i < l; i++){
+            float x1 = pointList.get(i).x;
+            float y1 = pointList.get(i).y;
 
-                // check if segments between p1&p2 and p3&p4 have intersection
-                boolean haveIntersection = doIntersect(p1, p2, p3, p4);
+            for(int j = 0; j < l; j++){
+                float x2 = pointList.get(j).x;
+                float y2 = pointList.get(j).y;
 
-                // 繋ぎなおし用メソッド
-                if(haveIntersection){
-                    fixPath(p1, p2, p3, p4);
-                }
-
-                next = next.nextNode;
+                distances[i][j] = calcDistance(x1, y1, x2, y2);
             }
-
         }
-        return nodeList;
+        return distances;
+    }
+
+    public void removeIntersection(List<Node> nodes){
+        //パラメーター不要？
+        List<Node> nodeList = nodes;
+
+        for (int i = 0; i < nodeList.size(); i++) {
+            Node current = nodeList.get(i);
+            Node next = current.nextNode;
+
+            for (int j = i + 2; j < nodeList.size(); j++) { // start from i+2 to avoid adjacent nodes
+                Node compareCurrent = nodeList.get(j);
+                Node compareNext = compareCurrent.nextNode;
+
+                Point p1 = pointList.get(current.current);
+                Point p2 = pointList.get(next.current);
+                Point p3 = pointList.get(compareCurrent.current);
+                Point p4 = pointList.get(compareNext.current);
+
+                if (doIntersect(p1, p2, p3, p4)) {
+                    fixPath(current, next, compareCurrent, compareNext);
+                    break; // Exit the inner loop to start checking for new intersections, but shouldn't?
+                }
+            }
+        }
+
     }
 
     // ** NOT WORKING PROPERLY NOW **
-    public void fixPath(Point p1, Point p2,Point p3, Point p4){
-        //始点から始点、終点から終点へ繋ぎ直す？
+    // will update to doubly linked list to memorise previous node enabling switching directions
+    public void fixPath(Node node1, Node node2, Node node3, Node node4){
         // change path as below
         // p1 -> p2, p3 -> p4  >>>  p1 -> p3, p2 -> p4
-        int index = pointList.indexOf(p1);
+
+        // Reconnect to remove the intersection
+        Node temp = node2.nextNode;
+        node2.nextNode = node4;
+        node4.nextNode = temp;
+
+        temp = node3.nextNode;
+        node3.nextNode = node1;
+        node1.nextNode = temp;
+    }
+
+    public ArrayList<Integer> findShortestPath(float[][] distances){
+        ArrayList<Integer> nodeList = new ArrayList<>();
+        nodeList.add(0);
+        int current = 0;
+        ArrayList<Integer> nonVisited = new ArrayList<>();
+        int len = pointList.size();
+
+        // Create non-visited nodes list filled with 1-n(length)
+        for(int i = 1; i < len; i++){
+            nonVisited.add(i);
+        }
+
+        for(int i = 0; i < len; i++){  //currentから伸びる距離の最短探し
+            //ArrayList<Integer> nextOpt = new ArrayList<>();
+
+            float minDistance = Integer.MAX_VALUE;
+            int nextNode = 0;
+
+            for(int n : nonVisited){  //未探索地のみをループさせる
+                if(i != n && distances[current][n] < minDistance){
+                    minDistance = distances[current][n];
+                    nextNode = n;
+                }
+                //TODO: 枝分かれさせたい！
+                // 新メソッドで現時点でのnodeListとnonVisitedを渡して、続きを書いてもらう。その中で一番距離の短いものを選択？or同メソッドを再度使用する？
+                // Add nodes to a list if the shortest distance duplicates to make branches
+            }
+
+            nonVisited.remove(nextNode);
+            nodeList.add(current);
+            current = nextNode;
+        }
+
+        return nodeList;
+    }
+
+    /*
+    public ArrayList<Integer> findShortestRecursively(ArrayList<Integer> nodeList, ArrayList<Integer> nonVisited){
 
     }
+     */
+
+    public ArrayList<Integer> modifyPath(ArrayList<Integer> firstPath, float[][] distances){
+
+        ArrayList<Integer> modifiedPath = firstPath;
+        int len = firstPath.size();
+
+        // Check if the edge containing i-st node has intersects by trying to reconnect to each node
+        for(int i = 0; i < len; i++){
+            int firstStartNode = firstPath.get(i);
+            int firstEndNode = firstPath.get(i+1);
+            float edgeLength = distances[firstStartNode][firstEndNode];
+
+            int targetStartIndex = i + 2;
+            int targetEndIndex = i + 3;
+
+            while(targetEndIndex != 0){  // O(N) = O(len)
+                if(targetEndIndex == len) targetStartIndex = 0;  // return to the first node at the last
+
+                int secondStartNode = firstPath.get(targetStartIndex);
+                int secondEndNode = firstPath.get(targetEndIndex);
+
+                //compare original path length and switched path length
+                float originalLength = edgeLength + distances[secondStartNode][secondEndNode];
+                float switchedLength = distances[firstStartNode][secondStartNode] + distances[firstEndNode][secondEndNode];
+
+                if(originalLength > switchedLength){
+                    firstPath.set(i + 1, secondStartNode);
+                    firstPath.set(targetStartIndex, firstEndNode);
+                    break;
+                }
+                targetStartIndex++;
+                targetEndIndex++;
+            }
+
+        }
+
+        return modifiedPath;
+    }
+
+
 
     public List<Node> greedyAlgoSearch() {
 
@@ -90,9 +199,6 @@ public class SalesMan {
         int currentNode = 0;
         int[] visited = new int[length];  // change value to 1 once visited (indexes are corresponding to node nums)
 
-        // linked list to obtain path at the end
-        List<Node> nodeList = new ArrayList<>();
-        nodeList.add(head);
         Node prevNode = head;
 
         // Calculate to find the closest node from the current node,
@@ -100,8 +206,8 @@ public class SalesMan {
         for(int i = 0; i < length; i++){
             float x1 = pointList.get(currentNode).x;
             float y1 = pointList.get(currentNode).y;
-            float shortest = Integer.MAX_VALUE;
-            int nextNode = 0;  // 0 will be used to return to the initial node
+            float minDistance = Integer.MAX_VALUE;
+            int closestNode = 0;  // 0 will be used to return to the initial node
             visited[currentNode] = 1;
 
             // Record the shortest path between current node and others
@@ -110,24 +216,36 @@ public class SalesMan {
                     float x2 = pointList.get(j).x;
                     float y2 = pointList.get(j).y;
 
-                    float ans = (float) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
-                    if (ans < shortest) {
-                        shortest = ans;
-                        nextNode = j;
+                    float ans = calcDistance(x1, y1, x2, y2);
+                    if (ans < minDistance) {
+                        minDistance = ans;
+                        closestNode = j;
                     }
                     //else if(ans == shortest){
                     // What if some have the same distance??
                     //}
                 }
             }
-            Node node = new Node(currentNode, nextNode, shortest);  //nextNodeとshortest(distance)だけで事足りる?
+            Node node = new Node(currentNode, closestNode, minDistance);  //nextNodeとshortest(distance)だけで事足りる?
             prevNode.nextNode = node;
             prevNode = node;
-            nodeList.add(node);
-            currentNode = nextNode;
+            //nodeList.add(node);
+            currentNode = closestNode;
         }
 
+        // linked list to obtain path at the end
+        List<Node> nodeList = new ArrayList<>();
+        Node current = head;
+        do {
+            nodeList.add(current);
+            current = current.nextNode;
+        } while (current != head);
+
         return nodeList;
+    }
+
+    public float calcDistance(float x1, float y1, float x2, float y2){
+        return (float) Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
     }
 
     // Method to check if two line segments intersect or a point lies on a line segment
